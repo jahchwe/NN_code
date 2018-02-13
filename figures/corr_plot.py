2484f4618433e1c525a3ecf5c99d9a473f0ca9b3
marigalicer <marigalicer@gmail.com>
2018-02-13 17:30:20 -0500

    kmeans sorting

--- figures/corr_plot.py
+++ figures/corr_plot.py
@@ -5,15 +5,48 @@ from scipy.spatial.distance import squareform, pdist
 from scipy.stats import spearmanr
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.cluster import KMeans
 
 labels = ['aggressive','caring','confident','dominant','emotionally stable','intelligent','mean','responsible','sociable','trustworthy','unhappy','weird']
 data = genfromtxt('TLr1_subjectiveDM.csv', delimiter=',', skip_header=1)
 data = np.delete(data, 0, 1)
 
+estimator = KMeans(n_clusters=3)
+estimator.fit(data)
+cluster_grouping = {i: np.where(estimator.labels_ == i)[0] for i in range(estimator.n_clusters)}
+
+# create the reordered indices based on clustering
+new_order = []
+for i in cluster_grouping:
+  for n in cluster_grouping[i].tolist():
+    new_order.append(n)
+
+reordered_labels = []
+for i in new_order:
+  reordered_labels.append(labels[i])
+
+# reorder the rows based on clustering
+clustered_corrs = []
+for i in new_order:
+  clustered_corrs.append(data[i])
+
+# reorder columns based on clustering
+count = 0;
+for row in clustered_corrs:
+  new_row = [];
+  for col in new_order:
+    new_row.append(row[col])
+  clustered_corrs[count] = new_row
+  count = count + 1;
+
+# put labels back
+clustered_corrs = reordered_labels + clustered_corrs
+
 cmap = sns.diverging_palette(220, 10, as_cmap=True)
 
-cg = sns.clustermap(data, center=0, cmap=cmap)
-cg.ax_row_dendrogram.set_visible(False)
-cg.ax_col_dendrogram.set_visible(False)
+fig, axs = plt.subplots(figsize=(11, 9))
+
+sns.heatmap(clustered_corrs, cmap=cmap)
+
 plt.show()
 
