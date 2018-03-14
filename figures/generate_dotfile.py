c532a1b00c4fe32fde077468a09552185921fb77
marigalicer <marigalicer@gmail.com>
2018-03-13 21:52:30 -0700

    print out actual cluster names

--- figures/generate_dotfile.py
+++ figures/generate_dotfile.py
@@ -6,14 +6,20 @@ from sklearn import cluster
 from sklearn.cluster import KMeans
 
 # read from file
-labels = ['aggressive','caring','confident','dominant','emotionally stable','intelligent','mean','responsible','sociable','trustworthy','unhappy','weird']
-data = genfromtxt('data/conceptualDM.csv', delimiter=',', skip_header=1)
+labels = ['aggressive','caring','confident','dominant','egotistic','emotionally stable','intelligent','mean','responsible','sociable','trustworthy','unhappy','weird']
+
+data = genfromtxt('data/conceptSM.csv', delimiter=',', skip_header=1)
 my_data = np.delete(data, 0, 1)
 
 dt = [('len',  float)]
-# create values for length of nodes and pen width
 
+# create values for length of nodes and pen width
 # dt = np.dtype({'names': ['len', 'weight'], 'formats':[float, float]})
+print(my_data)
+
+for row in range (len(my_data)):
+    for col in range(len(my_data[0])):
+      my_data[row][col]+=1
 my_data = np.array(my_data, dt)
 
 G = nx.from_numpy_matrix(my_data)
@@ -21,10 +27,15 @@ G = nx.relabel_nodes(G, dict(zip(range(len(G.nodes())),labels)))
 G = nx.drawing.nx_agraph.to_agraph(G)
 
 # generate dotfile
-G.write('dotfiles/clustering_conceptualDM.dot')
+G.write('dotfiles/conceptSM.dot')
 
 estimator = KMeans(n_clusters=3)
 estimator.fit(my_data)
 cluster_grouping = {i: np.where(estimator.labels_ == i)[0] for i in range(estimator.n_clusters)}
 
-print(cluster_grouping)
+named_clusters = [];
+for x in range (len(cluster_grouping)):
+  for y in cluster_grouping[x]:
+    named_clusters.append(labels[y])
+
+print(named_clusters)
